c2b4f36f83808a6bbed169b544f3fb0c97838070
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2023-11-09 17:56:01 +0100

    page details

---
+++ details.js
@@ -0,0 +1,46 @@
+const params = new URLSearchParams(window.location.search);
+const newId = params.get("Id");
+console.log(newId);
+const client = " fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY ";
+const URL = "https://api.pexels.com/v1/photos/";
+
+window.onload = () => {
+  generatePage();
+};
+
+const generatePage = () => {
+  fetch(URL + newId, {
+    headers: {
+      Authorization: client,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      const container = document.querySelector(".container");
+      const link = document.createElement("a");
+      link.onclick = function info() {
+        window.location.assign(data.photographer_url);
+      };
+      const h1 = document.createElement("h1");
+      h1.innerText = `${data.photographer}`;
+      const img = document.createElement("img");
+      img.className = ("border border-secondary")
+      img.src = data.src.large;
+      const h4 = document.createElement("h4");
+      h4.innerText = `${data.alt}`;
+      const button = document.createElement("button");
+      button.innerText = "Back Home";
+      button.className = "btn btn-outline-secondary";
+      button.onclick = function back() {
+        window.location.assign("./Starting-template.html");
+      };
+
+      container.appendChild(link);
+      link.appendChild(h1);
+      container.appendChild(img);
+      container.appendChild(h4);
+      container.appendChild(button);
+    })
+    .catch((err) => console.log(err));
+};
--- index.js
+++ index.js
@@ -1,137 +1,133 @@
-const URL = "https://api.pexels.com/v1/search"
+const URL = "https://api.pexels.com/v1/search";
 
-window.onload = () =>{
-    const firstBtn = document.querySelector(".btn-primary")
-    const secondBtn = document.querySelector(".btn-secondary")
-    firstBtn.onclick = takePicture
-    secondBtn.onclick = takeNewPicture
-    
-    const form = document.querySelector("form")
-    form.onsubmit = search
-}
+window.onload = () => {
+  const firstBtn = document.querySelector(".btn-primary");
+  const secondBtn = document.querySelector(".btn-secondary");
+  firstBtn.onclick = takePicture;
+  secondBtn.onclick = takeNewPicture;
 
+  const form = document.querySelector("form");
+  form.onsubmit = search;
+};
 
-const search = (e) =>{
-    e.preventDefault()
-    const client =' fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY '; 
-    const query = document.querySelector("#photo").value
-    console.log(query);
-    fetch(URL + "?query=" + query, {
-        method: "GET",
-        headers: {
-            Authorization: client
-        }
-    }).then(resp => resp.json())
-    .then( obj => {
-        generateCard(obj);
-        console.log(obj)
-    });
-}
+const search = (e) => {
+  e.preventDefault();
+  const client = " fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY ";
+  const query = document.querySelector("#photo").value;
+  console.log(query);
+  fetch(URL + "?query=" + query, {
+    method: "GET",
+    headers: {
+      Authorization: client,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((obj) => {
+      generateCard(obj);
+      console.log(obj);
+    })
+    .catch((err) => console.log(err));
+};
 
+const takePicture = () => {
+  const client = " fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY ";
+  const query = " Natura ";
 
-const takePicture = () =>{
-    const client =' fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY '; 
-    const query = ' Natura ' ;
-    
-    fetch(URL + "?query=" + query, {
-        method: "GET",
-        headers: {
-            Authorization: client
-        }
-    }).then(resp => resp.json())
-    .then( obj => {
-        generateCard(obj);
-        console.log(obj)
-    });
-    
-    
-}
-const takeNewPicture = () =>{
-    const client =' fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY '; 
-    const query = ' sea ' ;
-    
-    fetch(URL + "?query=" + query, {
-        method: "GET",
-        headers: {
-            Authorization: client
-        }
-    }).then(resp => resp.json())
-    .then( obj => {
-        generateCard(obj);
-        console.log(obj)
-    });
-}
+  fetch(URL + "?query=" + query, {
+    method: "GET",
+    headers: {
+      Authorization: client,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((obj) => {
+      generateCard(obj);
+      console.log(obj);
+    })
+    .catch((err) => console.log(err));
+};
+const takeNewPicture = () => {
+  const client = " fV4jS8Yae1kHqWB7G2Jvy2SbOBnEpfViY5gOqsILhmEJpDIIwUKmXCGY ";
+  const query = " sea ";
 
+  fetch(URL + "?query=" + query, {
+    method: "GET",
+    headers: {
+      Authorization: client,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((obj) => {
+      generateCard(obj);
+      console.log(obj);
+    })
+    .catch((err) => console.log(err));
+};
 
-const generateCard = (x) =>{
-    const container = document.querySelector(".container-md");
-    container.remove()
-    const newContainer = document.createElement("div")
-    newContainer.className = "container-md"
-    const row = document.createElement("div")
-    row.className = "row"
-    x.photos.forEach(element => {
-        const col = document.createElement("div")
-        col.className = "col-4"
-        const card = document.createElement("div")
-        card.className = "card mb-4 shadow-sm"
-        const linkI = document.createElement("a")
-        linkI.onclick = function info() {
-            window.location.assign ("./details.html?Id=" + element.id)
-        }
-        const img = document.createElement("img")
-        img.className = "card-img-top"
-        img.src = element.src.medium
-        const cardBody = document.createElement("div")
-        cardBody.className = "card-body"
-        const linkP = document.createElement("a")
-        linkP.onclick = function info() {
-            window.location.assign ("./details.html?Id=" + element.id)
-        }
-        const h5 = document.createElement("h5")
-        h5.className = "card-title"
-        h5.innerText = `${element.photographer}`
-        const p = document.createElement("p")
-        p.className = "card-text"
-        p.innerText = `${element.alt}`
-        const div = document.createElement("div")
-        div.className = "d-flex justify-content-between align-items-center"
-        const divB = document.createElement("div")
-        divB.className = "btn-group"
-        const buttonV = document.createElement("button")
-        buttonV.className = "btn btn-sm btn-outline-secondary"
-        buttonV.innerText = "View"
-        const buttonH = document.createElement("button")
-        buttonH.className = "btn btn-sm btn-outline-secondary"
-        buttonH.innerText = "Hide"
-        buttonH.onclick = () =>{
-            col.remove()
-        }
-        const small = document.createElement("small")
-        small.className = "text-muted"
-        small.innerText = `${element.id}`
-        
-        
-        
-        
-        
-        card.appendChild(linkI)
-        linkI.appendChild(img)
-        card.appendChild(cardBody)
-        cardBody.appendChild(linkP)
-        linkP.appendChild(h5)
-        cardBody.appendChild(p)
-        cardBody.appendChild(div)
-        div.appendChild(divB)
-        div.appendChild(small)
-        divB.appendChild(buttonV)
-        divB.appendChild(buttonH)
-        col.appendChild(card)
-        row.appendChild(col)
-    });
-    
-    const album = document.querySelector(".album")
-    newContainer.appendChild(row);
-    album.appendChild(newContainer)
-    
-}
+const generateCard = (x) => {
+  const container = document.querySelector(".container-md");
+  container.remove();
+  const newContainer = document.createElement("div");
+  newContainer.className = "container-md";
+  const row = document.createElement("div");
+  row.className = "row";
+  x.photos.forEach((element) => {
+    const col = document.createElement("div");
+    col.className = "col-4";
+    const card = document.createElement("div");
+    card.className = "card mb-4 shadow-sm";
+    const linkI = document.createElement("a");
+    linkI.onclick = function info() {
+      window.location.assign("./details.html?Id=" + element.id);
+    };
+    const img = document.createElement("img");
+    img.className = "card-img-top";
+    img.src = element.src.medium;
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    const linkP = document.createElement("a");
+    linkP.onclick = function info() {
+      window.location.assign("./details.html?Id=" + element.id);
+    };
+    const h5 = document.createElement("h5");
+    h5.className = "card-title";
+    h5.innerText = `${element.photographer}`;
+    const p = document.createElement("p");
+    p.className = "card-text";
+    p.innerText = `${element.alt}`;
+    const div = document.createElement("div");
+    div.className = "d-flex justify-content-between align-items-center";
+    const divB = document.createElement("div");
+    divB.className = "btn-group";
+    const buttonV = document.createElement("button");
+    buttonV.className = "btn btn-sm btn-outline-secondary";
+    buttonV.innerText = "View";
+    const buttonH = document.createElement("button");
+    buttonH.className = "btn btn-sm btn-outline-secondary";
+    buttonH.innerText = "Hide";
+    buttonH.onclick = () => {
+      col.remove();
+    };
+    const small = document.createElement("small");
+    small.className = "text-muted";
+    small.innerText = `${element.id}`;
+
+    card.appendChild(linkI);
+    linkI.appendChild(img);
+    card.appendChild(cardBody);
+    cardBody.appendChild(linkP);
+    linkP.appendChild(h5);
+    cardBody.appendChild(p);
+    cardBody.appendChild(div);
+    div.appendChild(divB);
+    div.appendChild(small);
+    divB.appendChild(buttonV);
+    divB.appendChild(buttonH);
+    col.appendChild(card);
+    row.appendChild(col);
+  });
+
+  const album = document.querySelector(".album");
+  newContainer.appendChild(row);
+  album.appendChild(newContainer);
+};
